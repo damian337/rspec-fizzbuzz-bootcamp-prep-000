97edadda7fc5feb67543a35bfad52dc8db03f1f3
Damian Guillory <guillorywdamian@gmail.com>
2017-10-22 21:00:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
     if int % 3 == 0
-        "Fizz"
+        return "Fizz"
     elsif int % 5 == 0
-        "Buzz"
+        return "Buzz"
     elsif int % 3 == 0 && int % 5 == 0
-        "FizzBuzz"
+        return "FizzBuzz"
 end
